7f6e567d13f88e7a944c62251415f8e999732b85
luckt <lucktao8@163.com>
2022-04-20 16:52:26 +0800

    remove .pyc file

--- scrapypra/spiders/girlygirlpic.py
+++ scrapypra/spiders/girlygirlpic.py
@@ -17,7 +17,7 @@ class GirlygirlpicSpider(scrapy.Spider):
         payload = {
             "action": "load_infinite_content",
             #paged分页
-            "next_params": "paged=4&posts_per_page=10&post_status=publish&category__in=0",
+            "next_params": "paged=1&posts_per_page=10&post_status=publish&category__in=0",
             "layout_type": "v3",
             "template_type": "",
             "page_id": "pid407",
@@ -36,7 +36,7 @@ class GirlygirlpicSpider(scrapy.Spider):
             'content-type': 'application/json',
             'cookie': '_ga=GA1.1.260901069.1650075980; _user_language=En; _clck=1uakkkb|1|f0p|0; dom3ic8zudi28v8lr6fgphwffqoz0j6c=84e367bc-743f-4486-802c-9f82594cbc42%3A1%3A1; _ga_1T7ZPY6GGW=GS1.1.1650188587.6.0.1650188587.0; zone-cap-4420168=1; _clsk=ds1d3k|1650188667916|1|1|b.clarity.ms/collect; _user_language=En',
             'origin': 'https://en.girlygirlpic.com',
-            'referer': 'https://en.girlygirlpic.com/l/6imhhs2',
+            # 'referer': 'https://en.girlygirlpic.com/l/6imhhs2',
             'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"macOS"',
@@ -47,8 +47,9 @@ class GirlygirlpicSpider(scrapy.Spider):
             'x-requested-with': 'XMLHttpRequest'
         }
 
-
-        yield Request(url,self.getpageurl,headers=headers,method="POST",body=json.dumps(payload))
+        if payload['next_params'] is not None:
+            #注意传入payload使用的方法
+            yield Request(url,self.getpageurl,headers=headers,method="POST",body=json.dumps(payload))
         
     def getpageurl(self,response):
         jsonresponse=response.json()
@@ -103,146 +104,4 @@ class GirlygirlpicSpider(scrapy.Spider):
           'image_urls':img_src,
           'image_name':image_name
         }
-    
-    
-        
-    # def urlsparse(self,response):
-    #     #当前页 所有具体modal链接地址
-    #     raw_url=response.xpath("//a[@class='on-popunder figure-link']/@href").getall()
-        
-    #     for cla_url in raw_url:
-    #         url = "https://en.girlygirlpic.com/ax/"
-            
-    #         payloadid=cla_url.split('/')[-1]
-            
-    #         payload = {
-                
-    #             "album_id": payloadid
-    #         }
-    #         headers = {
-    #             'authority': 'en.girlygirlpic.com',
-    #             'accept': '*/*',
-    #             'accept-language': 'zh-CN,zh;q=0.9',
-    #             'content-type': 'application/json',
-    #             'cookie': '_ga=GA1.1.260901069.1650075980; _user_language=En; _ga_1T7ZPY6GGW=GS1.1.1650105311.4.1.1650106636.0; sb_main_3954c4e9eb36a3ec329b506677cf5f13=1; sb_count_3954c4e9eb36a3ec329b506677cf5f13=1; zone-cap-4420168=1; _clck=1uakkkb|1|f0p|0; _clsk=1ejejkg|1650178406405|1|1|l.clarity.ms/collect; _user_language=En',
-    #             'origin': 'https://en.girlygirlpic.com',
-    #             # 'referer': 'https://en.girlygirlpic.com/a/6s1w6dc',
-    #             'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-    #             'sec-ch-ua-mobile': '?0',
-    #             'sec-ch-ua-platform': '"macOS"',
-    #             'sec-fetch-dest': 'empty',
-    #             'sec-fetch-mode': 'cors',
-    #             'sec-fetch-site': 'same-origin',
-    #             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
-    #             'x-requested-with': 'XMLHttpRequest'
-    #         }
-    #         yield Request(url,self.parse,headers=headers,method="POST",body=json.dumps(payload))
-            
- 
-        
-    # # 
-    # def start_requests(self):
-    #     url = "https://en.girlygirlpic.com/lx/"
-
-    #     payload ={
-    #         "country_id": "6imhhs2"
-    #     }
-    #     headers = {
-    #         'authority': 'en.girlygirlpic.com',
-    #         'accept': '*/*',
-    #         'accept-language': 'zh-CN,zh;q=0.9',
-    #         'content-type': 'application/json',
-    #         'cookie': '_ga=GA1.1.260901069.1650075980; _user_language=En; sb_main_3954c4e9eb36a3ec329b506677cf5f13=1; _clck=1uakkkb|1|f0p|0; sb_count_3954c4e9eb36a3ec329b506677cf5f13=4; dom3ic8zudi28v8lr6fgphwffqoz0j6c=84e367bc-743f-4486-802c-9f82594cbc42%3A1%3A1; zone-cap-4420168=1; _clsk=kcde51|1650180006133|1|1|d.clarity.ms/collect; _ga_1T7ZPY6GGW=GS1.1.1650179996.5.1.1650180015.0; _user_language=En',
-    #         'origin': 'https://en.girlygirlpic.com',
-    #         'referer': 'https://en.girlygirlpic.com/l/6imhhs2',
-    #         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-    #         'sec-ch-ua-mobile': '?0',
-    #         'sec-ch-ua-platform': '"macOS"',
-    #         'sec-fetch-dest': 'empty',
-    #         'sec-fetch-mode': 'cors',
-    #         'sec-fetch-site': 'same-origin',
-    #         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
-    #         'x-requested-with': 'XMLHttpRequest'
-    #     }
-    #     yield Request(url,self.urlsparse,headers=headers,method="POST",body=json.dumps(payload))
-    #   #  
-    # def urlsparse(self,response):
-    #     #当前页 所有具体modal链接地址
-    #     raw_url=response.xpath("//a[@class='on-popunder figure-link']/@href").getall()
-        
-    #     for cla_url in raw_url:
-    #         url = "https://en.girlygirlpic.com/ax/"
-            
-    #         payloadid=cla_url.split('/')[-1]
-            
-    #         payload = {
-                
-    #             "album_id": payloadid
-    #         }
-    #         headers = {
-    #             'authority': 'en.girlygirlpic.com',
-    #             'accept': '*/*',
-    #             'accept-language': 'zh-CN,zh;q=0.9',
-    #             'content-type': 'application/json',
-    #             'cookie': '_ga=GA1.1.260901069.1650075980; _user_language=En; _ga_1T7ZPY6GGW=GS1.1.1650105311.4.1.1650106636.0; sb_main_3954c4e9eb36a3ec329b506677cf5f13=1; sb_count_3954c4e9eb36a3ec329b506677cf5f13=1; zone-cap-4420168=1; _clck=1uakkkb|1|f0p|0; _clsk=1ejejkg|1650178406405|1|1|l.clarity.ms/collect; _user_language=En',
-    #             'origin': 'https://en.girlygirlpic.com',
-    #             # 'referer': 'https://en.girlygirlpic.com/a/6s1w6dc',
-    #             'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-    #             'sec-ch-ua-mobile': '?0',
-    #             'sec-ch-ua-platform': '"macOS"',
-    #             'sec-fetch-dest': 'empty',
-    #             'sec-fetch-mode': 'cors',
-    #             'sec-fetch-site': 'same-origin',
-    #             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
-    #             'x-requested-with': 'XMLHttpRequest'
-    #         }
-    #         yield Request(url,self.parse,headers=headers,method="POST",body=json.dumps(payload))
-            
-    # def parse(self, response):
-    #     # print(response.body)
-    #     #图片地址
-    #     img=response.xpath('//div[@class="figure-link-w"]/a/@href').getall()
-    #     # print(img)
-        
-    #     yield {
-    #     'image_urls':img
-    #     }
-  
-        
-        
-
-    # #from最终页面download images
-    # def start_requests(self):
-        
-    #     url = "https://en.girlygirlpic.com/ax/"
-
-    #     payload = {
-    #         "album_id": "6s1w6dc"
-    #     }
-    #     headers = {
-    #         'authority': 'en.girlygirlpic.com',
-    #         'accept': '*/*',
-    #         'accept-language': 'zh-CN,zh;q=0.9',
-    #         'content-type': 'application/json',
-    #         'cookie': '_ga=GA1.1.260901069.1650075980; _user_language=En; _ga_1T7ZPY6GGW=GS1.1.1650105311.4.1.1650106636.0; sb_main_3954c4e9eb36a3ec329b506677cf5f13=1; sb_count_3954c4e9eb36a3ec329b506677cf5f13=1; zone-cap-4420168=1; _clck=1uakkkb|1|f0p|0; _clsk=1ejejkg|1650178406405|1|1|l.clarity.ms/collect; _user_language=En',
-    #         'origin': 'https://en.girlygirlpic.com',
-    #         'referer': 'https://en.girlygirlpic.com/a/6s1w6dc',
-    #         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-    #         'sec-ch-ua-mobile': '?0',
-    #         'sec-ch-ua-platform': '"macOS"',
-    #         'sec-fetch-dest': 'empty',
-    #         'sec-fetch-mode': 'cors',
-    #         'sec-fetch-site': 'same-origin',
-    #         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
-    #         'x-requested-with': 'XMLHttpRequest'
-    #     }
-    #     yield Request(url,self.parse,headers=headers,method="POST",body=json.dumps(payload))
-        
-
-    # def parse(self, response):
-    #     #图片地址
-    #     img=response.xpath('//div[@class="figure-link-w"]/a/@href').getall()
-        
-    #     yield {
-    #        'image_urls':img
-    #     }
+   
